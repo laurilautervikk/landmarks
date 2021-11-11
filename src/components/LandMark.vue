6486b5bf6f257aabbbca771e8280c7a55f72b1bb
EdgarKol <90323766+EdgarKol@users.noreply.github.com>
2021-11-11 20:03:38 +0200

    landing page

---
+++ src/components/LandMark.vue
@@ -0,0 +1,72 @@
+
+<template>
+<div class="container">
+  <div class="row row-cols-4">
+    <div class="col">
+    <a class="col" href="#"><img src="https://www.worldatlas.com/r/w1200/upload/56/fb/ee/alaska-mountain-range-csnafzger.jpg" alt="landmark logo"></a>
+    <h4>Landmark 1</h4>
+    </div>
+     <div class="col">
+    <a class="col" href="#"><img src="https://www.worldatlas.com/r/w1200/upload/56/fb/ee/alaska-mountain-range-csnafzger.jpg" alt="landmark logo"></a>
+    <h4>Landmark 1</h4>
+    </div>
+    <div class="col">
+    <a class="col" href="#"><img src="https://www.worldatlas.com/r/w1200/upload/56/fb/ee/alaska-mountain-range-csnafzger.jpg" alt="landmark logo"></a>
+    <h4>Landmark 1</h4>
+    </div>
+    <div class="col">
+    <a class="col" href="#"><img src="https://www.worldatlas.com/r/w1200/upload/56/fb/ee/alaska-mountain-range-csnafzger.jpg" alt="landmark logo"></a>
+    <h4>Landmark 1</h4>
+    </div>
+</div>
+<div class="row row-cols-4">
+    <div class="col">
+    <a class="col" href="#"><img src="https://www.worldatlas.com/r/w1200/upload/56/fb/ee/alaska-mountain-range-csnafzger.jpg" alt="landmark logo"></a>
+    <h4>Landmark 1</h4>
+    </div>
+     <div class="col">
+    <a class="col" href="#"><img src="https://www.worldatlas.com/r/w1200/upload/56/fb/ee/alaska-mountain-range-csnafzger.jpg" alt="landmark logo"></a>
+    <h4>Landmark 1</h4>
+    </div>
+    <div class="col">
+    <a class="col" href="#"><img src="https://www.worldatlas.com/r/w1200/upload/56/fb/ee/alaska-mountain-range-csnafzger.jpg" alt="landmark logo"></a>
+    <h4>Landmark 1</h4>
+    </div>
+    <div class="col">
+    <a class="col" href="#"><img src="https://www.worldatlas.com/r/w1200/upload/56/fb/ee/alaska-mountain-range-csnafzger.jpg" alt="landmark logo"></a>
+    <h4>Landmark 1</h4>
+    </div>
+</div>
+
+</div>
+</template>
+
+<script>
+
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h4 {
+  margin-bottom: 30px;
+  margin-top: 20px;
+}
+/* col {
+  margin-top: 30px;
+ margin-bottom: 20px;
+ height: 250px;
+ width: 250px;
+} */
+img {
+  
+  margin-top: 20px;
+  max-height: 250px;
+  max-width: 250px;
+
+}
+.container {
+  margin-top: 120px;
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,18 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <LandMark msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+
+import LandMark from "@/components/LandMark.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
-  },
+    LandMark,
+},
 };
 </script>
