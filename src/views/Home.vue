f36c636659fc23a14ee16f1c5f70d5568b8ef8f1
EdgarKol <90323766+EdgarKol@users.noreply.github.com>
2021-11-14 20:19:13 +0200

    lisasin imagegridi

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,32 @@
 <template>
   <div class="home">
     <landMarks msg="Welcome to Your Vue.js App" />
+    <Imagegrid msg="Message from Home.vue" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
-import landMarks from "../components/landMarks.vue";
+import landMarks from "@/components/landMarks.vue";
+import Imagegrid from "@/components/Imagegrid.vue";
 
 export default {
   name: "Home",
   components: {
     landMarks,
 },
+export default {
+  name: "Home",
+  components: {
+    Imagegrid,
+  },
+  data() {
+    return {
+      id: 0,
+    };
+  },
+},
 };
+
 </script>
