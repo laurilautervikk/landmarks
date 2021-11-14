87909915283d39ecac6fe34d9ad221ea46e3e184
EdgarKol <90323766+EdgarKol@users.noreply.github.com>
2021-11-14 19:37:46 +0200

    changed name landmark to Imagegrid

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <LandMark msg="Welcome to Your Vue.js App" />
+    <Imagegrid msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
-import LandMark from "@/components/LandMark.vue";
+import Imagegrid from "@/components/Imagegrid.vue";
 
 export default {
   name: "Home",
   components: {
-    LandMark,
+    Imagegrid,
   },
 };
 </script>
