af062b5a644d8af41f48fdabcd1459c31685fcff
EdgarKol <90323766+EdgarKol@users.noreply.github.com>
2021-12-19 22:13:08 +0200

    checks tokens if token is expired

--- src/main.js
+++ src/main.js
@@ -2,5 +2,19 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import axios from "axios";
+
+
+axios.interceptors.response.use(undefined, function(error) {
+    if (error) {
+        const originalRequest = error.config;
+        if (error.response.status === 401 && !originalRequest._retry) {
+            originalRequest._retry = true;
+            store.dispatch("LogOut");
+            console.log('Token expired')
+            return router.push("/login");
+        }
+    }
+});
 
 createApp(App).use(store).use(router).mount("#app");
