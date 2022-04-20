57760c907e23269d41006c1202808659fb8be45e
laurilautervikk <lauri.lauter@vikk.ee>
2022-04-20 17:58:56 +0300

    env added

--- server/dbConnection.js
+++ server/dbConnection.js
@@ -1,13 +1,10 @@
+require("env2")(".env");
 const mongoose = require("mongoose");
 const { Landmarks } = require("./models/LandmarksSchema.js");
 const { Users } = require("./models/UsersSchema.js");
 
 // Connecting to database
-mongoose.connect(
-  "mongodb+srv://hapukapsas:hapukapsas@cluster1.ckjul.mongodb.net/landmarksapp?retryWrites=true&w=majority",
-  { useNewUrlParser: true }
-);
-
+mongoose.connect(process.env.DATABASE, { useNewUrlParser: true });
 
 module.exports = {
   Landmarks,
