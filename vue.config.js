ff81d7079150685b1afa0a78ece29f20f1e5a89c
laurilautervikk <lauri.lauter@vikk.ee>
2022-04-20 18:12:14 +0300

    Added CI

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,18 @@
+/* module.exports = {
+  devServer: {
+    port: 8080,
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+      },
+    },
+  },
+}; */
+
 module.exports = {
-    "devServer": {
-      "port": 8080,
-      "proxy": {
-        "/api": {
-          "target": "http://localhost:3000",
-          "changeOrigin": true
-        }
-      }
-    }
-  }
+  publicPath:
+    process.env.NODE_ENV === "production"
+      ? "/" + process.env.LANDMARKS + "/"
+      : "/",
+};
